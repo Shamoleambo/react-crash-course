68f625d5d52c169f4257ca138d3fdb40fad9508c
Tiago Gomes <tidgomes@gmail.com>
2021-12-07 16:14:13 -0300

    toggle add task form

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -5,6 +5,8 @@ import AddTask from "./components/AddTask";
 import { useState } from "react";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -37,6 +39,10 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  const toggleAddTask = (e) => {
+    setShowAddTask(!showAddTask);
+  };
+
   const toggleReminder = (id) => {
     setTasks(
       tasks.map((task) =>
@@ -47,8 +53,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onClick={toggleAddTask} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
