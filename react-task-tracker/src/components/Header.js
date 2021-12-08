9b8672e518d6cd2cd1a8f4560f03dc76f257b43d
Tiago Gomes <tidgomes@gmail.com>
2021-12-08 09:52:12 -0300

    usage of Link instead of <a> in Footer and About - Edit of the button in the Header component with useLocation for the path

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,16 +1,21 @@
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 
 import Button from "./Button";
 
 const Header = ({ title, onClick, showAdd }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAdd ? "red" : "green"}
-        text={showAdd ? "Close" : "Add"}
-        onClick={onClick}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onClick}
+        />
+      )}
     </header>
   );
 };
